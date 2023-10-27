2368e95fab1be71b05816f2d0b9a7cdaac6d9338
Thomas Topfstedt <thotop100@gmail.com>
2023-10-27 07:54:16 -0300

    pt-br

--- topicos-especiais-mecatronica.ino
+++ topicos-especiais-mecatronica.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-// Defining the pins for the LEDs of the traffic lights
+// Definindo os pinos para os LEDs dos semáforos
 const int red_led1 = 2;
 const int yellow_led1 = 3;
 const int green_led1 = 4;
@@ -9,13 +9,13 @@ const int red_led2 = 5;
 const int yellow_led2 = 6;
 const int green_led2 = 7;
 
-// Defining the sensors
-// Assuming that weight and photoelectric sensors return digital values
+// Definindo os sensores
+// Supondo que os sensores de peso e fotoelétricos retornem valores digitais
 const int pedestrian_sensor = 8;
 const int vehicle_sensor = 9;
 
 void setup() {
-  // Initialize the LEDs as outputs
+  // Inicializar os LEDs como saídas
   pinMode(red_led1, OUTPUT);
   pinMode(yellow_led1, OUTPUT);
   pinMode(green_led1, OUTPUT);
@@ -24,16 +24,16 @@ void setup() {
   pinMode(yellow_led2, OUTPUT);
   pinMode(green_led2, OUTPUT);
   
-  // Initialize the sensors as inputs
+  // Inicializar os sensores como entradas
   pinMode(pedestrian_sensor, INPUT);
   pinMode(vehicle_sensor, INPUT);
   
-  // Initialize the serial port for debugging
+  // Inicializar a porta serial para depuração
   Serial.begin(9600);
 }
 
 void mode1() {
-  Serial.println("Mode 1 Activated");
+  Serial.println("Modo 1 Ativado");
   digitalWrite(red_led1, HIGH);
   digitalWrite(red_led2, LOW);
   delay(5000);
@@ -62,26 +62,26 @@ void mode1() {
 }
 
 void mode2() {
-  Serial.println("Mode 2 Activated");
-  // Code to prioritize pedestrians
+  Serial.println("Modo 2 Ativado");
+  // Código para dar prioridade aos pedestres
 }
 
 void mode3() {
-  Serial.println("Mode 3 Activated");
-  // Code to prioritize vehicle traffic
+  Serial.println("Modo 3 Ativado");
+  // Código para dar prioridade ao tráfego de veículos
 }
 
 void loop() {
   int pedestrian_count = digitalRead(pedestrian_sensor);
   int vehicle_count = digitalRead(vehicle_sensor);
   
-  if(pedestrian_count > 10) {  // Assuming 10 is the limit
+  if(pedestrian_count > 10) {  // Supondo que 10 seja o limite
     mode2();
-  } else if(vehicle_count > 10) {  // Assuming 10 is the limit
+  } else if(vehicle_count > 10) {  // Supondo que 10 seja o limite
     mode3();
   } else {
     mode1();
   }
   
-  delay(1000);  // Wait for 1 second before making the next reading
+  delay(1000);  // Aguardar 1 segundo antes de fazer a próxima leitura
 }
